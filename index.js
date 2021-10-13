9e59e8ccca39ceb66159ce6500a092529f0137cb
Yogas <ramayogaz@gmai.com>
2021-10-13 22:14:01 +0800

    Added fully responsive rev

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//
 function increment() {
   document.getElementById("numbernya").stepUp();
 }
 function decrement() {
   document.getElementById("numbernya").stepDown();
 }
+
+let hamburger = document.getElementsById("hamburger");
+let nav = document.getElementById("navgone");
+
+hamburger.addEventLister("click", () => {
+  navgone.classList.toggle("show");
+});
